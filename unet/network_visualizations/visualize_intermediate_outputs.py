a6b1b269b1d57f55eca515939dca93d670ee0ca2
maurbe <mauro.bernardini@uzh.ch>
2019-10-01 12:25:59 +0200

    Version 2.0 + added visualization scripts and folder + massively updated all scripts and entire project - todo: rewrite of overplot_helper for incorporating mass correction

--- unet/network_visualizations/visualize_intermediate_outputs.py
+++ unet/network_visualizations/visualize_intermediate_outputs.py
@@ -12,10 +12,9 @@ from unet.metrics.custom_metrics import selective_mse, selective_r2_score, r2_sc
 from unet.generator.datagen import custom_DataGenerator
 
 from matplotlib.colors import LinearSegmentedColormap
-mcmap = LinearSegmentedColormap.from_list('mycmap', ['#3F1F47', '#5C3C9A', '#6067B3', '#6067B3', '#5C3C9A', '#45175D', '#2F1435',
-                                                     '#601A49', '#8C2E50', '#A14250', '#B86759', '#E0D9E1'][::-1])
 
 homedir = os.path.dirname(os.getcwd()) + '/'
+projectdir = os.path.dirname(os.path.dirname(os.getcwd())) + '/'
 input_shape = (128, 128, 128, 1)
 test_ids  = np.arange(0, 1, 1)
 datagen_params_test = {'dim': input_shape[0],
@@ -43,31 +42,36 @@ net.compile('adam', loss=selective_mse, metrics=['mse',
 layer_outputs = [layer.output for layer in net.layers[1:]] # HAVE TO DO [1:] TO LEAVE OUT INPUT LAYER!
 activation_model = Model(inputs=net.input, outputs=layer_outputs) # Creates a model that will return these outputs, given the model input
 
-x = np.load(homedir+'data/training/x_0.npy')
-y = np.load(homedir+'data/training/y_0.npy')
+x = np.load(homedir+'data/training/x_160.npy')
+y = np.load(homedir+'data/training/y_160.npy')
 
 activations = activation_model.predict(x[np.newaxis, ...]) # Returns a list of five Numpy arrays: one array per layer activation
 print(len(activations))
 imgs = []
+
 plt.figure()
-plt.imshow(x[:, :, 50, 0], cmap='magma')
+plt.imshow(x[:, :, 50, 0], cmap='magma', clim=(-4, 4))
 plt.axis('off')
+plt.savefig(projectdir + 'draw.io/input.png', dpi=150)
+
 for i, activation in enumerate(activations):
     print(activation.shape)
     if i in [4, 10, 16, 22, 28,
              33, 41, 49, 57, 65, 74]:
         if i in [33, 41, 49, 57, 65, 74]:
-            colormap = mcmap
+            colormap = 'twilight_r'
         else:
             colormap = 'magma'
         plt.figure()
         plt.imshow(activation[0, :, :, np.rint(50.0/128.0*activation.shape[3]).astype(int), 0], cmap=colormap)
         print(np.rint(50/128.0*activation.shape[3]).astype(int))
         plt.axis('off')
+        plt.savefig(projectdir + 'draw.io/{}.png'.format(i), dpi=150)
 
 plt.figure()
-plt.imshow(y[:, :, 50, 0], cmap=mcmap)
+plt.imshow(y[:, :, 50, 0], cmap='twilight_r')
 plt.axis('off')
+plt.savefig(projectdir + 'draw.io/target.png', dpi=150)
 plt.show()
 
 
